a995d12c7969031b6b32dc0863a2ad2310d4a260
lexsander ikhaylov <mihaleksval@gmail.com>
2021-07-12 19:55:20 +0500

    delete

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,35 +3,16 @@ import React from 'react';
 class Actions extends React.Component {
   constructor(props) {
     super(props);
-    this.props = {
-      isChecked: null,
-    };
+    this.state = {};
   }
-  componentDidMount() {
-    this.setState({
-      isChecked: this.props.isChecked,
-    });
-  }
-  _handleChange() {
-    this.setState({
-      isChecked: !this.props.isChecked,
-    });
-  }
+  handleClick;
   render() {
-    const power = () => {};
     return (
       <div className='container-right' id='display'>
         <div className='switch-button flex justify-center pl-0 pr-5 mt-20'>
           <p className=''>POWER</p>
         </div>
-        <div
-          ref='switch'
-          checked={this.props.isChecked}
-          onChange={this._handleChange}
-          className='switch'
-          type='checkbox'
-          style={{ float: 'right' }}
-        ></div>
+        <div className='switch' style={{ float: 'right' }}></div>
         <hr />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Actions from './actions/actions';
-import './styles/app.css';
+import "./styles/app.css"
+
 
 ReactDOM.render(
-  <App>
-    <Actions isChecked={true} />
-  </App>,
+    <App />,
   document.getElementById('root')
 );

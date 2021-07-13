4a4c4dcaaf554afe4c2ae24824aa68279e61c063
lexsander ikhaylov <mihaleksval@gmail.com>
2021-07-13 23:29:41 +0500

    cosmetic

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -4,10 +4,58 @@ import 'tailwindcss/tailwind.css';
 
 const collectionOne = [
   {
-    keyTrigger: 'Q',
     sound:
       'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/80[kb]african-pe-hi.wav.mp3',
-    label: 'african-hi',
+    label: 'Q',
+    id: 'african-hi ',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/90[kb]african-pe-lo.wav.mp3',
+    label: 'W',
+    id: 'african-lo ',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/91[kb]african-pe-med.wav.mp3',
+    label: 'E',
+    id: 'african-med',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/171[kb]bendy.wav.mp3',
+    label: 'A',
+    id: 'bendy',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/114[kb]bigbend.wav.mp3',
+    label: 'S',
+    id: 'bigbend',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/36[kb]blobble.wav.mp3',
+    label: 'D',
+    id: 'blobble',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/12[kb]bongo%20hi.wav.mp3',
+    label: 'Z',
+    id: 'bongo-hi',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/27[kb]bongo%20lo.wav.mp3',
+    label: 'X',
+    id: 'bongo-lo',
+  },
+  {
+    sound:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/69[kb]crikix.wav.mp3',
+    label: 'C',
+    id: 'crikix ',
   },
 ];
 
@@ -22,139 +70,27 @@ class Buttons extends React.Component {
   RenderSound = () => {
     return collectionOne.map((soundObj, index) => {
       return (
-        <button key={index} onClick={() => this.SoundPlay(soundObj.sound)}>
+        <button
+          className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg button-active'
+          key={index}
+          onClick={() => this.SoundPlay(soundObj.sound)}
+        >
           {soundObj.label}
         </button>
       );
     });
   };
   render() {
-    const buttontsPadOne = {
-      background: '#F87171',
-    };
     Howler.volume(1.0);
     return (
       <div className='container' id='display'>
-        <div className='grid grid-flow-col grid-cols-3 grid-rows-3 gap-4'>
-          <button
-            id='african-hi'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            Q{this.RenderSound()}
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/80[kb]african-pe-hi.wav.mp3'
-              id='Q'
-            >
-              {this.RenderSound()}
-            </audio>
-          </button>
-          <button
-            id='african-lo'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            W
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/90[kb]african-pe-lo.wav.mp3'
-              id='W'
-            ></audio>
-          </button>
-          <button
-            id='africa-med'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            E
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/91[kb]african-pe-med.wav.mp3'
-              id='E'
-            ></audio>
-          </button>
-          <button
-            id='bendy'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            A
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/171[kb]bendy.wav.mp3'
-              id='A'
-            ></audio>
-          </button>
-          <button
-            id='bigbend'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            S
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/114[kb]bigbend.wav.mp3'
-              id='S'
-            ></audio>
-          </button>
-          <button
-            id='blobble'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            D
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/36[kb]blobble.wav.mp3'
-              id='D'
-            ></audio>
-          </button>
-          <button
-            id='bongo hi'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            Z
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/12[kb]bongo%20hi.wav.mp3'
-              id='Z'
-            ></audio>
-          </button>
-          <button
-            id='bongo lo'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            X
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/27[kb]bongo%20lo.wav.mp3'
-              id='X'
-            ></audio>
-          </button>
-          <button
-            id='crikix'
-            className='w-full h-full text-sm font-bold text-red-medium p-10 shadow-2xl rounded-lg drum-pad'
-            type='button'
-            style={buttontsPadOne}
-          >
-            C
-            <audio
-              className='clip'
-              src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/69[kb]crikix.wav.mp3'
-              id='C'
-            ></audio>
-          </button>
+        <div className='grid grid-flow-col grid-cols-3 grid-rows-3 gap-4 drum-pad'>
+          {this.RenderSound()}
+          <audio
+            className='clip'
+            src='https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/80[kb]african-pe-hi.wav.mp3'
+            id='Q'
+          ></audio>
         </div>
       </div>
     );

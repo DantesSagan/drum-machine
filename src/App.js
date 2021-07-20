6fdc71ab454f93bbe078a8fbbe48a6d48bf21258
lexsander ikhaylov <mihaleksval@gmail.com>
2021-07-20 18:00:30 +0500

    second switch

--- src/App.js
+++ src/App.js
@@ -69,6 +69,63 @@ const collectionOne = [
   },
 ];
 
+const collectionTwo = [
+  {
+    keyCode: 81,
+    keyid: 'Q',
+    id: 'Chord-1',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+  },
+  {
+    keyCode: 87,
+    keyid: 'W',
+    id: 'Chord-2',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+  },
+  {
+    keyCode: 69,
+    keyid: 'E',
+    id: 'Chord-3',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+  },
+  {
+    keyCode: 65,
+    keyid: 'A',
+    id: 'Shaker',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+  },
+  {
+    keyCode: 83,
+    keyid: 'S',
+    id: 'Open-HH',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+  },
+  {
+    keyCode: 68,
+    keyid: 'D',
+    id: 'Closed-HH',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+  },
+  {
+    keyCode: 90,
+    keyid: 'Z',
+    id: 'Punchy-Kick',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+  },
+  {
+    keyCode: 88,
+    keyid: 'X',
+    id: 'Side-Stick',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+  },
+  {
+    keyCode: 67,
+    keyid: 'C',
+    id: 'Snare',
+    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+  },
+];
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export default function App() {
   const [active] = React.useState(false);
@@ -80,6 +137,8 @@ export default function App() {
   const [speed, setSpeed] = React.useState(0.5);
 
   const [power, setPower] = React.useState(true);
+
+  const [on, setOn] = React.useState(true);
 
   const playRecording = () => {
     let index = 0;
@@ -102,6 +161,28 @@ export default function App() {
       return (
         <div>
           {/* POWER */}
+          <div className='flex justify-center pl-0 pr-5 mt-5'>
+            <p className='text-center text-black text-2xl font-bold'>Switch</p>
+          </div>
+          <p className='text-center font-bold text-xs pr-5 mt-1'>Turn to On</p>
+          <div
+            className='switch-button flex justify-center pl-0 pr-80'
+            style={{
+              backgroundColor: '#fffdd0',
+              border: '1px solid black',
+              borderRadius: '30px',
+              width: '50',
+              height: '42px',
+              margin: '15px auto',
+            }}
+          >
+            <div // turn OFF
+              className='button-switch bg-red-600'
+              style={{ float: 'left' }}
+            >
+              <p className='text-center text-white font-bold '>OFF</p>
+            </div>
+          </div>
           <div className='md:max-w-2xl'>
             <div className='flex justify-center pl-0 pr-5 mt-5'>
               <p className='font-bold text-2xl'>Power</p>
@@ -130,26 +211,51 @@ export default function App() {
           </div>
 
           {/* SOUND */}
-          <div className='grid grid-flow-col grid-cols-3 grid-rows-3 gap-4'>
-            {collectionOne.map((sound) => (
+          <div className='grid grid-cols-3 grid-rows-3 gap-4'>
+            {collectionOne.map((s) => (
               <div className='text-center'>
                 <div
-                  id={sound.id}
+                  id={s.id}
                   type='button'
                   className={` bg-red-300 w-full h-full text-sm font-bold  p-10  button-active drum-pad`}
                 >
                   <audio className='clip'></audio>
-                  {sound.keyid}
+                  {s.keyid}
                 </div>
               </div>
             ))}
           </div>
         </div>
       );
-    } else {
+    } else if (on) {
       return (
         <div>
           {/* POWER */}
+          <div className='flex justify-center pl-0 pr-5 mt-5'>
+            <p className='text-center text-black text-2xl font-bold'>Switch</p>
+          </div>
+          <p className='text-center font-bold text-xs pr-5 mt-1'>
+            African Bongo
+          </p>
+          <div
+            className='switch-button flex justify-center pl-0 pr-80'
+            style={{
+              backgroundColor: '#fffdd0',
+              border: '1px solid black',
+              borderRadius: '30px',
+              width: '50',
+              height: '42px',
+              margin: '15px auto',
+            }}
+          >
+            <div // turn OFF
+              className='button-switch bg-red-600'
+              style={{ float: 'left' }}
+              onClick={() => setOn(!on)}
+            >
+              <p className='text-center text-white font-bold '>OFF</p>
+            </div>
+          </div>
           <div className='md:max-w-2xl'>
             <div className='flex justify-center pl-0 pr-5 mt-5'>
               <p className='font-bold text-2xl'>Power</p>
@@ -190,6 +296,75 @@ export default function App() {
           </div>
         </div>
       );
+    } else {
+      return (
+        <div>
+          {/* POWER */}
+          <div className='flex justify-center pl-0 pr-5 mt-5'>
+            <p className='text-center text-black text-2xl font-bold'>Switch</p>
+          </div>
+          <p className='text-center font-bold text-xs pr-5 mt-1'>
+            Piano && Drum
+          </p>
+          <div
+            className='switch-button flex justify-center pl-80 pr-0'
+            style={{
+              backgroundColor: '#fffdd0',
+              border: '1px solid black',
+              borderRadius: '30px',
+              width: '50',
+              height: '42px',
+              margin: '15px auto',
+            }}
+          >
+            <div // turn ON
+              className='button-switch bg-green-700'
+              style={{ float: 'right' }}
+              onClick={() => setOn(!on)}
+            >
+              <p className='text-center text-white font-bold '>ON</p>
+            </div>
+          </div>
+          <div className='md:max-w-2xl'>
+            <div className='flex justify-center pl-0 pr-5 mt-5'>
+              <p className='font-bold text-2xl'>Power</p>
+            </div>
+            <div
+              className='switch-button flex justify-center pl-80 pr-0'
+              style={{
+                backgroundColor: '#fffdd0',
+                border: '1px solid black',
+                borderRadius: '30px',
+                width: '50',
+                height: '42px',
+                margin: '15px auto',
+              }}
+            >
+              <div // turn ON
+                className='button-switch bg-green-700'
+                style={{ float: 'right' }}
+                onClick={() => setPower(!power)}
+              >
+                <p className='text-center text-white font-bold '>ON</p>
+              </div>
+            </div>
+          </div>
+          <hr />
+
+          {/* SOUND */}
+          <div className='grid grid-flow-col grid-cols-3 grid-rows-3 gap-4'>
+            {collectionTwo.map((sndObj) => (
+              <Pad
+                power={power}
+                key={sndObj.id}
+                soundObj={sndObj}
+                volume={volume}
+                setRecording={setRecording}
+              />
+            ))}
+          </div>
+        </div>
+      );
     }
   };
 
@@ -197,11 +372,16 @@ export default function App() {
     // MAIN DRUM-MACHINE
     <div
       id='drum-machine'
-      className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden d-flex p-4 grid grid-flow-col grid-cols-1 gap-4 mt-20 '
+      className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden d-flex p-4 grid grid-flow-col grid-cols-1 gap-4 mt-16 '
       style={{ background: '#FECACA' }}
     >
       {/* RECORDING */}
       <div className='md:max-w-2xl'>
+        {' '}
+        <h1 className='text-4xl font-bold text-center mt-5 mb-5'>
+          Drum Machine
+        </h1>
+        <hr />
         <div
           id='display'
           className='switch-button justify-center pl-0 pr-5'
@@ -238,7 +418,6 @@ export default function App() {
         </div>
         <hr />
         <p className='text-center font-bold text-xs'>In progress #1</p>
-
         {/* VOLUME */}
         <h4 className='flex justify-center pl-0 pr-5 font-bold text-2xl'>
           Volume
@@ -255,32 +434,14 @@ export default function App() {
           />
           <p className='font-bold'> {volume}</p>
         </div>
-        <div className='text-center'></div>
-        <div className='flex justify-center pl-0 pr-5 mt-5'>
-          <p className='text-center text-black text-2xl font-bold'>Switch</p>
-        </div>
-        <p className='text-center font-bold text-xs'>In progress #2</p>
-        <div
-          className='switch-button flex justify-center pl-80 pr-0'
-          style={{
-            backgroundColor: '#fffdd0',
-            border: '1px solid black',
-            borderRadius: '30px',
-            width: '50',
-            height: '42px',
-            margin: '15px auto',
-          }}
-        >
-          <div // turn ON
-            className='button-switch bg-green-700'
-            style={{ float: 'right' }}
-          >
-            <p className='text-center text-white font-bold '>ON</p>
-          </div>
-        </div>
         <hr />
-        {swtichClick(power)}
+        {swtichClick()}
       </div>
     </div>
   );
 }
+function confirmEnding(str, target) {
+  return str.substr(-target.length) === target;
+}
+
+console.log(confirmEnding('Bastian', 'n'));
